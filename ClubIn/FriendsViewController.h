3180aee564feecab4423649fddc1d4834f990258
Cameron Bradley <cameronbradley.git@gmail.com>
2012-06-14 20:54:03 +1000

    Friends loaded into UITableView (within UIViewController)
    Loading View (via tableView hiding) created to handle waiting for FBook
    API didLoad method in order to populate Table View.

--- ClubIn/FriendsViewController.h
+++ ClubIn/FriendsViewController.h
@@ -9,9 +9,28 @@
 #import <UIKit/UIKit.h>
 #import "FBConnect.h"
 #import "AppDelegate.h"
+#import "PullRefreshTableViewController.h"
 
-@interface FriendsViewController : UITableViewController <FBSessionDelegate, FBDialogDelegate, FBRequestDelegate> {
+@interface FriendsViewController : UIViewController <FBSessionDelegate, FBDialogDelegate, FBRequestDelegate, UITableViewDataSource, UITableViewDelegate> {
     
+    //array to store the API graph results for me/friends.
+    NSArray *items;
+    
+    //store facebook friends Id in NSMutableArrays
+    NSMutableArray *friendIds;
+    NSMutableArray *friendNames;
+    
+    //UIElements for..
+    UILabel *loadingLabel;
+    UITableView *menuTableView;
+    UIActivityIndicatorView *activityIndicator;    
 }
+
+@property (nonatomic, retain) NSArray *items;
+@property (nonatomic, retain) NSMutableArray *friendIds;
+@property (nonatomic, retain) NSMutableArray *friendNames;
+@property (nonatomic, retain) UILabel *loadingLabel;
+@property (nonatomic, retain) UIActivityIndicatorView *activityIndicator;
+@property (nonatomic, retain) UITableView *menuTableView;
 
 @end
--- ClubIn/FriendsViewController.m
+++ ClubIn/FriendsViewController.m
@@ -10,13 +10,128 @@
 
 @implementation FriendsViewController
 
+@synthesize friendIds;
+@synthesize friendNames;
+@synthesize items;
+@synthesize loadingLabel;
+@synthesize activityIndicator;
+@synthesize menuTableView;
+
+-(void)dealloc {
+    [friendIds release];
+    [friendNames release];
+    [items release];
+    [loadingLabel release];
+    [activityIndicator release];
+    [menuTableView release];
+    [super dealloc];
+}
+
+//initilize view programmatically
+- (void) loadView {
+    
+    //allocate view
+    UIView *view = [[UIView alloc] initWithFrame:[UIScreen
+                                                  mainScreen].applicationFrame];
+    
+    //add background default_bg image to view
+    view.backgroundColor = [UIColor whiteColor];
+    
+    self.view = view;
+    [view release];
+    
+    //allocate Table View to view and hide it on load.
+    menuTableView = [[UITableView alloc] initWithFrame:self.view.bounds
+                                                 style:UITableViewStylePlain];
+    [menuTableView setBackgroundColor:[UIColor whiteColor]];
+    menuTableView.separatorStyle = UITableViewCellSeparatorStyleSingleLine;
+    menuTableView.autoresizingMask =  UIViewAutoresizingFlexibleWidth;
+    menuTableView.dataSource = self;
+    menuTableView.delegate = self;
+    menuTableView.hidden = YES;
+    [self.view addSubview:menuTableView];
+    
+    // loading label display on load
+    loadingLabel = [[UILabel alloc]
+                    initWithFrame:CGRectMake(7.0, 2.5, 85.0, 40.0)];
+    loadingLabel.textAlignment = UITextAlignmentCenter;
+    loadingLabel.textColor = [UIColor blueColor];
+    loadingLabel.backgroundColor = [UIColor clearColor];
+    //set text as selected club.
+    loadingLabel.text = @"Loading....";
+    //add lblClubLabel to headbar subview.
+    [self.view addSubview:loadingLabel];
+    
+    // Activity Indicator display on load.
+    activityIndicator = [[UIActivityIndicatorView alloc] initWithFrame:CGRectMake(92.0, 3.5, 30, 30)];
+    activityIndicator.activityIndicatorViewStyle = UIActivityIndicatorViewStyleGray;
+    //add activity indicator to subview
+    [self.view addSubview:activityIndicator];
+    //call method to show the activity indicator on load.
+    [self showActivityIndicator];
+}
 
 -(void)viewDidLoad {
-    
+
     //set the navigation bar item title.
-    self.navigationItem.title = @"Fellow Clubbers";
+    self.navigationItem.title = @"Friends";
     
+    //graph the logged in users friends.
     [self apiGraphFriends];
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    
+}
+
+//This method hides the table view (for showing loading)
+- (void)showLoadingView {
+    
+    //hide the UITableView.
+    menuTableView.hidden = YES;
+    
+    //show the loading label.
+    loadingLabel.hidden = NO;
+    
+    //show the activity indicator.
+    [self showActivityIndicator];
+    
+    //reload the table view.
+    [self.menuTableView reloadData];
+    
+}
+
+//This method shows the table view (when data is loaded)
+- (void)hideLoadingView {
+    
+    //show the UITableView
+    menuTableView.hidden = NO;
+    
+    //hide the loading label.
+    loadingLabel.hidden = YES;
+    
+    //hide the activity indicator.
+    [self hideActivityIndicator];
+    
+    //reload the table view.
+    [self.menuTableView reloadData];
+}
+
+//This method shows the activity indicator
+- (void)showActivityIndicator {
+    if (![activityIndicator isAnimating]) {
+        [activityIndicator startAnimating];
+    }
+}
+
+//This method hides the activity indicator
+- (void)hideActivityIndicator {
+    if ([activityIndicator isAnimating]) {
+        [activityIndicator stopAnimating];
+    }
+    
 }
 
 #pragma mark - Facebook API Calls
@@ -28,109 +143,84 @@
     // Do not set current API as this is commonly called by other methods
     AppDelegate *delegate = (AppDelegate *)[[UIApplication sharedApplication] delegate];
     [[delegate facebook] requestWithGraphPath:@"me/friends" andDelegate:self];
-
 }
 
 - (void)request:(FBRequest *)request didLoad:(id)result {
-    NSLog(@"%@", result);
+    /*https://graph.facebook.com/me/friends?limit=5000&offset=0&format=json&access_token=BAADAMvDi7X0BAK2SErYztSTsvKjuQqMkWbdk364Xib4WNPAZAJceZCwIoeMM6ZCZAwQNnEpszCokHsStJKtlpO56jULLoyFAcBrn4zvQi1iaIdF4YZBtxIcjEmx6jyKoZD*/
+
+    //allocate NSMutableArrays
+    friendIds = [[NSMutableArray alloc] init];
+    friendNames = [[NSMutableArray alloc] init];
     
-    if ([result isKindOfClass:[NSArray class]] && ([result count] > 0)) {
-        result = [result objectAtIndex:0];
+    items = [[(NSDictionary *)result objectForKey:@"data"]retain];
+    for (int i=0; i<[items count]; i++) {
+        NSDictionary *friend = [items objectAtIndex:i];
+        [friendIds addObject:[friend objectForKey:@"id"]];
+        [friendNames addObject:[friend objectForKey:@"name"]];
     }
-    
-    int test = [result count];
-    
-    if ([result count] > 0) {
-        NSLog(@"found friends%i ", test);
-    } else {
-        NSLog(@"no friends found");
-    }
+
+    //hide the activity indicator
+    [self hideLoadingView];    
 }
 
-- (void)viewDidUnload
-{
-    [super viewDidUnload];
-
-}
-
-- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
-{
-    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+//adjust height for table cell.
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
+    //height of cell
+    return 80.0;
 }
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
 {
     // Return the number of sections.
-    return 0;
+    return 1;
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
     // Return the number of rows in the section.
-    return 0;
+    return [friendIds count];
 }
+
+//table view cell styles.
+-(void)tableView:(UITableView *)tableView willDisplayCell:(UITableViewCell *)cell forRowAtIndexPath:(NSIndexPath *)indexPath {
+    
+    //set the background image for the cell.
+    cell.backgroundColor = [UIColor whiteColor];
+    
+    //textLabel styles.
+    cell.textLabel.backgroundColor = [UIColor clearColor];
+    cell.textLabel.font = [UIFont systemFontOfSize:16.0];
+    cell.textLabel.textColor = [UIColor blueColor];
+    
+    //detailTextLabel styles.
+    cell.detailTextLabel.backgroundColor = [UIColor clearColor];
+    cell.detailTextLabel.font = [UIFont systemFontOfSize:10.0];
+    cell.detailTextLabel.textColor = [UIColor blueColor];
+}
+
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    static NSString *CellIdentifier = @"Cell";
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"MyIdentifier"];
+    if (cell == nil) {
+        cell = [[[UITableViewCell alloc] initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:@"MyIdentifier"] autorelease];
+        //set Selection style (when button is pressed).
+        cell.selectionStyle = UITableViewCellSelectionStyleNone;
+        //set the accessory type (the button on the cell) to none.
+        cell.accessoryType = UITableViewCellSelectionStyleNone;
+        
+    }
     
-    // Configure the cell...
+    //set the textLabel as the clubName.
+    cell.textLabel.text =  [friendNames objectAtIndex:indexPath.row];
     
     return cell;
 }
 
-/*
-// Override to support conditional editing of the table view.
-- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
 {
-    // Return NO if you do not want the specified item to be editable.
-    return YES;
-}
-*/
-
-/*
-// Override to support editing the table view.
-- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    if (editingStyle == UITableViewCellEditingStyleDelete) {
-        // Delete the row from the data source
-        [tableView deleteRowsAtIndexPaths:[NSArray arrayWithObject:indexPath] withRowAnimation:UITableViewRowAnimationFade];
-    }   
-    else if (editingStyle == UITableViewCellEditingStyleInsert) {
-        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-    }   
-}
-*/
-
-/*
-// Override to support rearranging the table view.
-- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath
-{
-}
-*/
-
-/*
-// Override to support conditional rearranging of the table view.
-- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    // Return NO if you do not want the item to be re-orderable.
-    return YES;
-}
-*/
-
-#pragma mark - Table view delegate
-
-- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    // Navigation logic may go here. Create and push another view controller.
-    /*
-     <#DetailViewController#> *detailViewController = [[<#DetailViewController#> alloc] initWithNibName:@"<#Nib name#>" bundle:nil];
-     // ...
-     // Pass the selected object to the new view controller.
-     [self.navigationController pushViewController:detailViewController animated:YES];
-     [detailViewController release];
-     */
+    //only allow portrait orientation.
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
 }
 
 @end
