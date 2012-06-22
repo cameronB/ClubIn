4158c9b54515a40a8eb4cb0916ef2e1414b73bf8
Cameron Bradley <cameronbradley.git@gmail.com>
2012-06-23 01:47:46 +1000

    Friends View TabBar (significant coding)
    Added top tab bar (inside app)(outside app) for returning friends (not
    fully implemented)
    Returning Facebook friends, fields installed/picture/defaults
    Loading and refreshing changed.

--- ClubIn/FriendsViewController.h
+++ ClubIn/FriendsViewController.h
@@ -11,26 +11,41 @@
 #import "AppDelegate.h"
 #import "PullRefreshTableViewController.h"
 
-@interface FriendsViewController : UIViewController <FBRequestDelegate, UITableViewDataSource, UITableViewDelegate> {
+@interface FriendsViewController : UIViewController <FBRequestDelegate, UITableViewDataSource, UITableViewDelegate, UITabBarDelegate> {
     
     //array to store the API graph results for me/friends.
     NSArray *items;
+    BOOL friendUsesApp;
     
     //store facebook friends Id in NSMutableArrays
     NSMutableArray *friendIds;
     NSMutableArray *friendNames;
+    NSMutableArray *friendInstalled;
+    NSMutableArray *friendPicture;
     
-    //UIElements for..
+    //UIObjects
     UILabel *loadingLabel;
     UITableView *menuTableView;
-    UIActivityIndicatorView *activityIndicator;    
+    UIActivityIndicatorView *activityIndicator; 
+    UITabBar *loadingTabBar;
+    UITabBar *friendsTabBar;
+    UITabBarItem *friendsUsingTabBarItem;
+    UITabBarItem *friendsNotUsingTabBarItem;
+    
 }
 
 @property (nonatomic, retain) NSArray *items;
+@property (nonatomic) BOOL friendUsesApp;
 @property (nonatomic, retain) NSMutableArray *friendIds;
 @property (nonatomic, retain) NSMutableArray *friendNames;
+@property (nonatomic, retain) NSMutableArray *friendInstalled;
+@property (nonatomic, retain) NSMutableArray *friendPicture;
 @property (nonatomic, retain) UILabel *loadingLabel;
 @property (nonatomic, retain) UIActivityIndicatorView *activityIndicator;
 @property (nonatomic, retain) UITableView *menuTableView;
+@property (nonatomic, retain) UITabBar *loadingTabBar;
+@property (nonatomic, retain) UITabBar *friendsTabBar;
+@property (nonatomic, retain) UITabBarItem *friendsUsingTabBarItem;
+@property (nonatomic, retain) UITabBarItem *friendsNotUsingTabBarItem;
 
 @end
--- ClubIn/FriendsViewController.m
+++ ClubIn/FriendsViewController.m
@@ -12,18 +12,31 @@
 
 @synthesize friendIds;
 @synthesize friendNames;
+@synthesize friendInstalled;
+@synthesize friendPicture;
 @synthesize items;
+@synthesize friendUsesApp;
 @synthesize loadingLabel;
-@synthesize activityIndicator;
 @synthesize menuTableView;
+@synthesize activityIndicator;
+@synthesize loadingTabBar;
+@synthesize friendsTabBar;
+@synthesize friendsUsingTabBarItem;
+@synthesize friendsNotUsingTabBarItem;
 
 -(void)dealloc {
     [friendIds release];
     [friendNames release];
+    [friendInstalled release];
+    [friendPicture release];
     [items release];
     [loadingLabel release];
-    [activityIndicator release];
     [menuTableView release];
+    [activityIndicator release];
+    [loadingTabBar release];
+    [friendsTabBar release];
+    [friendsUsingTabBarItem release];
+    [friendsNotUsingTabBarItem release];
     [super dealloc];
 }
 
@@ -40,8 +53,31 @@
     self.view = view;
     [view release];
     
+    //loadingt tab bar while table view is reloading (prob not best way to do this lol)
+    loadingTabBar = [[UITabBar alloc] initWithFrame:CGRectMake(0.0, 0.0, self.view.bounds.size.width, 47.0)];
+    loadingTabBar.hidden = NO;
+    [self.view addSubview:loadingTabBar];
+    
+    //create top tab bar that will display friends items for table view data.
+    friendsTabBar = [[UITabBar alloc] initWithFrame:CGRectMake(0.0, 0.0, self.view.bounds.size.width, 47.0)];
+    friendsTabBar.delegate = self;
+    friendsTabBar.hidden = YES;
+    [self.view addSubview:friendsTabBar];
+    
+    //add tab bar items to tab bar
+    friendsUsingTabBarItem = [[UITabBarItem alloc] initWithTitle:@"Invite Out" image:[UIImage imageNamed:@"24_info.png"] tag:0];
+    friendsNotUsingTabBarItem = [[UITabBarItem alloc] initWithTitle:@"Invite to ClubIn"  image:[UIImage imageNamed:@"24_info.png"] tag:1];
+    NSArray *arrTabbarItems = [NSArray arrayWithObjects:friendsUsingTabBarItem,friendsNotUsingTabBarItem, nil];
+    [friendsTabBar setItems:arrTabbarItems];
+    
+    //set the default for tab bar items
+    [friendsTabBar setSelectedItem:[friendsTabBar.items objectAtIndex:0]];
+    
     //allocate Table View to view and hide it on load.
-    menuTableView = [[UITableView alloc] initWithFrame:self.view.bounds
+    CGFloat xLoginButtonOffset = self.view.bounds.size.width;
+    CGFloat yLoginButtonOffset = self.view.bounds.size.height;
+    
+    menuTableView = [[UITableView alloc] initWithFrame:CGRectMake(0.0, 47.0, yLoginButtonOffset, xLoginButtonOffset)
                                                  style:UITableViewStylePlain];
     [menuTableView setBackgroundColor:[UIColor whiteColor]];
     menuTableView.separatorStyle = UITableViewCellSeparatorStyleSingleLine;
@@ -55,20 +91,62 @@
     loadingLabel = [[UILabel alloc]
                     initWithFrame:CGRectMake(7.0, 2.5, 85.0, 40.0)];
     loadingLabel.textAlignment = UITextAlignmentCenter;
-    loadingLabel.textColor = [UIColor blueColor];
+    loadingLabel.textColor = [UIColor whiteColor];
     loadingLabel.backgroundColor = [UIColor clearColor];
     //set text as selected club.
     loadingLabel.text = @"Loading....";
     //add lblClubLabel to headbar subview.
     [self.view addSubview:loadingLabel];
+}
+
+- (void)tabBar:(UITabBar *)tabBar didSelectItem:(UITabBarItem *)item {
     
-    // Activity Indicator display on load.
-    activityIndicator = [[UIActivityIndicatorView alloc] initWithFrame:CGRectMake(92.0, 3.5, 30, 30)];
-    activityIndicator.activityIndicatorViewStyle = UIActivityIndicatorViewStyleGray;
-    //add activity indicator to subview
-    [self.view addSubview:activityIndicator];
-    //call method to show the activity indicator on load.
-    [self showActivityIndicator];
+    if (item.tag == 0) {
+        [self displayFriendsUsingClubIn];
+    } else if (item.tag == 1) {
+        [self displayFriendsNotUsingClubIn];
+    }
+    
+}
+
+- (void)displayFriendsUsingClubIn {
+    NSLog(@"Friends using clubin");
+    
+    //For now reloading and regraphing friends each time view is loaded.
+    [self showLoadingView];
+    
+    friendUsesApp = TRUE;
+    
+    //graph facebook friends of logged in user
+    [self apiGraphFriends];    
+}
+
+
+//This method shows the activity indicator
+- (void)showActivityIndicator {
+    if (![activityIndicator isAnimating]) {
+        [activityIndicator startAnimating];
+    }
+}
+
+//This method hides the activity indicator
+- (void)hideActivityIndicator {
+    if ([activityIndicator isAnimating]) {
+        [activityIndicator stopAnimating];
+    }
+}
+
+- (void)displayFriendsNotUsingClubIn {
+    NSLog(@"Friends not using ClubIn");
+    
+    //For now reloading and regraphing friends each time view is loaded.
+    [self showLoadingView];
+    
+    friendUsesApp = FALSE;
+    
+    //graph facebook friends of logged in user
+    [self apiGraphFriends];
+    
 }
 
 -(void)viewDidLoad {
@@ -76,15 +154,22 @@
     //set the navigation bar item title.
     self.navigationItem.title = @"Friends";
     
+    //allocate indicator
+    activityIndicator = [[UIActivityIndicatorView alloc] 
+                         initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleWhite];
+    
+    
     //create a button on the top bar to navigate to the map (for all clubs)
     UIBarButtonItem *reloadButton = [[UIBarButtonItem alloc] 
-                                  initWithTitle:@"Refresh"                                            
-                                  style:UIBarButtonItemStyleBordered 
-                                  target:self 
-                                  action:@selector(refreshFriends)];
+                                     initWithCustomView:activityIndicator];
     self.navigationItem.rightBarButtonItem = reloadButton;
+    
+
     //release mapButton
     [reloadButton release]; 
+    
+    //show the activity indicator on load
+    [self showActivityIndicator];
     
     //graph facebook friends of logged in user
     [self apiGraphFriends];
@@ -113,11 +198,16 @@
     //hide the UITableView.
     menuTableView.hidden = YES;
     
+    [self showActivityIndicator];
+    
+    //show the loading tabbar
+    loadingTabBar.hidden = NO;
+    
+    //hide the top tabbar.
+    friendsTabBar.hidden = YES;
+    
     //show the loading label.
     loadingLabel.hidden = NO;
-    
-    //show the activity indicator.
-    [self showActivityIndicator];
     
     //reload the table view.
     [self.menuTableView reloadData];
@@ -130,29 +220,19 @@
     //show the UITableView
     menuTableView.hidden = NO;
     
+    [self hideActivityIndicator];
+    
+    //hide loading tab bar
+    loadingTabBar.hidden = YES;
+    
+    //show the top tab bar
+    friendsTabBar.hidden = NO;
+    
     //hide the loading label.
     loadingLabel.hidden = YES;
     
-    //hide the activity indicator.
-    [self hideActivityIndicator];
-    
     //reload the table view.
     [self.menuTableView reloadData];
-}
-
-//This method shows the activity indicator
-- (void)showActivityIndicator {
-    if (![activityIndicator isAnimating]) {
-        [activityIndicator startAnimating];
-    }
-}
-
-//This method hides the activity indicator
-- (void)hideActivityIndicator {
-    if ([activityIndicator isAnimating]) {
-        [activityIndicator stopAnimating];
-    }
-    
 }
 
 #pragma mark - Facebook API Calls
@@ -163,21 +243,29 @@
     NSLog(@"Graph user friends");
     // Do not set current API as this is commonly called by other methods
     AppDelegate *delegate = (AppDelegate *)[[UIApplication sharedApplication] delegate];
-    [[delegate facebook] requestWithGraphPath:@"me/friends" andDelegate:self];
+    [[delegate facebook] requestWithGraphPath:@"me/friends?fields=installed,name,picture" andDelegate:self];
 }
 
 - (void)request:(FBRequest *)request didLoad:(id)result {
-    /*https://graph.facebook.com/me/friends?limit=5000&offset=0&format=json&access_token=BAADAMvDi7X0BAK2SErYztSTsvKjuQqMkWbdk364Xib4WNPAZAJceZCwIoeMM6ZCZAwQNnEpszCokHsStJKtlpO56jULLoyFAcBrn4zvQi1iaIdF4YZBtxIcjEmx6jyKoZD*/
-
+    NSLog(@"%d", friendUsesApp);
+    
     //allocate NSMutableArrays
     friendIds = [[NSMutableArray alloc] init];
     friendNames = [[NSMutableArray alloc] init];
+    friendInstalled = [[NSMutableArray alloc] init];
+    friendPicture = [[NSMutableArray alloc] init];
     
     items = [[(NSDictionary *)result objectForKey:@"data"]retain];
     for (int i=0; i<[items count]; i++) {
         NSDictionary *friend = [items objectAtIndex:i];
         [friendIds addObject:[friend objectForKey:@"id"]];
         [friendNames addObject:[friend objectForKey:@"name"]];
+        //check if installed is nil
+        if ([friend objectForKey:@"installed"] != nil) {
+            //add object 'installed' to NSMutableArray.
+            [friendInstalled addObject:[friend objectForKey:@"installed"]];
+        }
+        [friendPicture addObject:[friend objectForKey:@"picture"]];
     }
     
     //hide the activity indicator
@@ -231,6 +319,12 @@
         cell.accessoryType = UITableViewCellSelectionStyleNone;
         
     }
+    
+    //Create image object / url of image got from me/friends facebook API.
+    UIImage *friendImage = [UIImage imageWithData:[NSData dataWithContentsOfURL:[NSURL URLWithString:[friendPicture objectAtIndex:indexPath.row]]]];
+
+    //set the imageView as the image
+    cell.imageView.image = friendImage;
     
     //set the textLabel as the clubName.
     cell.textLabel.text =  [friendNames objectAtIndex:indexPath.row];
